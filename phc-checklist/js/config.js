4f7d25e585c22dc4ae949133cdd9f3ed63d004d5
cherosli33-hub <cherosli33@gmail.com>
2026-08-05 12:49:21 +0800

    Aktifkan Google login admin dan penyelia

--- phc-checklist/js/config.js
+++ phc-checklist/js/config.js
@@ -1,7 +1,7 @@
 export const APP_VERSION = "2.7.0-supervisor";
 // Isi selepas OAuth Web Client dibuat. Nilai yang sama mesti disimpan sebagai
 // GOOGLE_OAUTH_CLIENT_ID dalam Apps Script > Project Settings > Script Properties.
-export const GOOGLE_OAUTH_CLIENT_ID = "";
+export const GOOGLE_OAUTH_CLIENT_ID = "655335153298-0cljukftkn09gsl2j2mcl758cmq9k5h7.apps.googleusercontent.com";
 
 // Isi URL deployment Google Apps Script selepas Sheet produksi dipasang
 // dalam akaun Google pemilik sistem.
